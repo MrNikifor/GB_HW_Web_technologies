94cd683e3d1bb21603e18ce756b5f52dae3c7bad
osian <oisa.nikifor@gmail.com>
2023-11-25 02:02:29 +0300

    HW_GB

---
+++ src/main/java/com/example/gb_hw_web_technologies/GbHwWebTechnologiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gb_hw_web_technologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbHwWebTechnologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbHwWebTechnologiesApplication.class, args);
+	}
+
+}
